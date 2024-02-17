9f6882a8a5f8a74ea4c40b8f1630308ea1030b82
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2024-02-17 13:08:39 -0300

    feat: carosuel done

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { apiClient } from "./apiClient";
 import { Movie } from "./types";
 import { Button } from "./components/ui/button";
 import { useState } from "react";
+import { cn } from "./utils";
 
 function App() {
   const { data: movies = [] } = useQuery({
@@ -20,49 +21,83 @@ function App() {
     <div className="flex flex-col items-center gap-10">
       <Input className="w-60" placeholder="Search movies..." />
 
-      <div className="flex overflow-hidden max-w-full relative">
-        <div
-          className="flex gap-2"
-          style={{
-            transform: `translateX(-${page * 15}%)`,
-            transition: "transform 0.6s",
-          }}
-        >
-          {movies.map((movie) => (
-            <div
-              className="h-[120px] min-w-[230px] bg-black flex items-center justify-center text-white"
-              key={movie.id}
-            >
-              <img
-                src={`https://api.themoviedb.org/3/movie/{movie_id}/images
-`}
-                alt={movie.title}
+      <div className="flex flex-col gap-2 max-w-full">
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl font-semibold">Popular</h2>
+          <div className="flex gap-2">
+            {Array.from({ length: 6 }).map((_, i) => (
+              <Button
+                key={`popular-pagination-${i}`}
+                className={cn(
+                  "h-2 rounded-full bg-white p-0 w-6",
+                  page === i && "bg-gray-500"
+                )}
+                onClick={() => setPage(i)}
               />
-              {movie.title}
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
 
-        {page > 0 && (
-          <Button
-            className="absolute left-0 h-full w-10 bg-transparent"
-            onClick={() => setPage((page) => page - 1)}
+        <div className="flex overflow-hidden max-w-full relative">
+          <div
+            className="flex"
+            style={{
+              transform: `translateX(-${page * 14.7}%)`,
+              transition: "transform 0.7s",
+            }}
           >
-            {"<"}
-          </Button>
-        )}
+            {movies.map((movie) => (
+              <CarsouselItem key={movie.id} movie={movie} />
+            ))}
+          </div>
 
-        {page < 5 && (
-          <Button
-            className="absolute right-0 h-full w-10"
-            onClick={() => setPage((page) => page + 1)}
-          >
-            {">"}
-          </Button>
-        )}
+          {page > 0 && (
+            <Button
+              className="absolute left- h-full w-10 text-3xl opacity-60"
+              onClick={() => setPage((page) => page - 1)}
+            >
+              {"<"}
+            </Button>
+          )}
+
+          {page < 5 && (
+            <Button
+              className="absolute right-0 h-full w-10 text-3xl opacity-60"
+              onClick={() => setPage((page) => page + 1)}
+            >
+              {">"}
+            </Button>
+          )}
+        </div>
       </div>
     </div>
   );
 }
+
+const CarsouselItem = ({ movie }: { movie: Movie }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div
+      className={cn(
+        "h-[360px] min-w-[230px] bg-black flex items-center justify-center text-white relative overflow-hidden",
+        isHovered && "opacity-60"
+      )}
+      onMouseEnter={() => {
+        setIsHovered(true);
+      }}
+      onMouseLeave={() => {
+        setIsHovered(false);
+      }}
+    >
+      <img
+        className="w-full h-full object-cover"
+        src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+        alt={movie.title}
+      />
+      <div className="absolute"></div>
+    </div>
+  );
+};
 
 export default App;

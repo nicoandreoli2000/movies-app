caa3fb58eed02e374a03e7a61ca7e0a65538ac83
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2024-02-16 21:12:20 -0300

    feat: configure everything

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Input } from "@/components/ui/input";
+
+function App() {
+  return (
+    <>
+      <Input placeholder="Search here..." />
+      <div className="h-[500px] w-full">Carousel</div>
+      <div className="h-10 w-full">Carousel</div>
+      <div className="h-10 w-full">Carousel</div>
+    </>
+  );
+}
+
+export default App;

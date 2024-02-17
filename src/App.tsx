15956c704ceba9b46276462a7cd97871bb3c4e8e
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2024-02-16 21:44:17 -0300

    feat: carousel going to

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
 import "./App.css";
 import { Input } from "@/components/ui/input";
+import { apiClient } from "./apiClient";
+import { Movie } from "./types";
 
 function App() {
+  const { data: movies = [] } = useQuery({
+    queryKey: ["movies"],
+    queryFn: async (): Promise<Movie[]> => {
+      return apiClient.get("/movie/popular").then(({ data }) => data.results);
+    },
+  });
+
   return (
-    <>
-      <Input placeholder="Search here..." />
-      <div className="h-[500px] w-full">Carousel</div>
-      <div className="h-10 w-full">Carousel</div>
-      <div className="h-10 w-full">Carousel</div>
-    </>
+    <div className="flex flex-col items-center gap-10">
+      <Input className="w-60" placeholder="Search here..." />
+      <div className="flex gap-2 overflow-hidden max-w-full">
+        {movies.map((movie) => (
+          <div
+            className="h-[120px] min-w-[230px] bg-black rounded-xl flex items-center justify-center text-white"
+            key={movie.id}
+          >
+            {movie.title}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
---
+++ src/apiClient.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const apiClient = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: "df506350a7840ac977e8c39b133d6d84",
+  },
+});
